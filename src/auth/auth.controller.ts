d682abb67d0f5d1e7794bb000d9559c63b9135f6
leelongxi <leelongxi@foxmail.com>
2025-04-20 18:46:49 +0800

    Init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2025-04-19 11:15:21
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2025-04-20 18:42:27
+ * @LastEditTime: 2025-04-20 18:46:03
  * @FilePath: /sbng_cake/shareholder_services/src/auth/auth.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
