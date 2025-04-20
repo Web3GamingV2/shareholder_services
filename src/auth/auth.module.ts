00df4244bb9ceb650e67501f40a691af5644eff2
leelongxi <leelongxi@foxmail.com>
2025-04-20 12:05:11 +0800

    Init

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: leelongxi leelongxi@foxmail.com
+ * @Date: 2025-04-19 11:15:18
+ * @LastEditors: leelongxi leelongxi@foxmail.com
+ * @LastEditTime: 2025-04-19 15:25:40
+ * @FilePath: /sbng_cake/shareholder_services/src/auth/auth.module.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
