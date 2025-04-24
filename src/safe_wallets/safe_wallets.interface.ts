0ac5185945d620fa6520cf4488dba73f6fec5b47
leelongxi <leelongxi@foxmail.com>
2025-04-24 20:18:25 +0800

    Init

--- src/safe_wallets/safe_wallets.interface.ts
+++ src/safe_wallets/safe_wallets.interface.ts
@@ -1,14 +1,12 @@
-import { OperationType } from "@safe-global/types-kit";
-
 /*
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2025-04-24 06:40:06
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2025-04-24 06:44:59
+ * @LastEditTime: 2025-04-24 17:14:18
  * @FilePath: /sbng_cake/shareholder_services/src/safe_wallets/safe_wallets.interface.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-interface CreateTransaction {
+export interface CreateTransaction {
   to: string;
-  value: string; 
+  value: string;
 }
